3f2a7f9df36a0ee07e10953f2475615facf770ee
Sang Kil Cha <sangkilc@kaist.ac.kr>
2021-03-07 00:12:49 +0900

    Init

---
+++ Library.fs
@@ -0,0 +1,4 @@
+module CS220.Library // This line declares Library module.
+
+let myfunc x =
+  x + x
---
+++ Program.fs
@@ -0,0 +1,11 @@
+module CS220.Program // This line declares Program module.
+
+open CS220.Library
+
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+[<EntryPoint>] // This line is essential for a program as it defines the main entry point of this program.
+let main argv =
+  let result = myfunc 42
+  printfn "%d" result
+  0 // DON't touch this; this is an integer exit code meaning successful termination.
