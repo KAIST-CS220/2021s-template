84f57a5a8413fb1748ec5ffedd6c5bbafd0f5c16
Sang Kil Cha <sangkilc@kaist.ac.kr>
2021-05-23 15:16:35 +0900

    Add splitWords

--- Library.fs
+++ Library.fs
@@ -1,4 +1,8 @@
 module CS220.Library // This line declares Library module.
 
+/// This function splits the given string into an array of words.
+let splitWords (str: string) =
+  System.Text.RegularExpressions.Regex.Split(str, @"\s+")
+
 let myfunc x y =
   x + y
